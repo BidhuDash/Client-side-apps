0f8c1825b0dda69479ceb4c6bd288b428546eb65
Bidhu <bbd.papun@gmail.com>
2023-12-20 01:01:48 +0530

    new changes

--- HelloAnglr/src/app/filter/filter.component.ts
+++ HelloAnglr/src/app/filter/filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-filter',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
+  All: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @Input('all') allProducts: number = 10;
+  @Input() available: number = 0;
+  @Input('not') notAvailable: number = 0;
+
+  selectedRadioButtonCount: string = 'All';
+
+  @Output()
+  filterRadioButtonSelectionChanged: EventEmitter<string> = new EventEmitter<string>();
+
+  onRadioButtonSelectionChange(){
+    this.filterRadioButtonSelectionChanged.emit(this.selectedRadioButtonCount);
+    //console.log(this.selectedRadioButtonCount);
+  }
 }
--- HelloAnglr/src/app/products/products.component.ts
+++ HelloAnglr/src/app/products/products.component.ts
@@ -13,16 +13,35 @@ export class ProductsComponent implements OnInit {
   }
 
   products = [
-    {id:1, name: 'Apple' , price:'50', available:'Available',image:'/assets/Products_Images/apple.jpg'},
-    {id:2, name: 'Egg' , price:'20', available:'Available',image:'/assets/Products_Images/eggs.webp'},
-    {id:3, name: 'Carrot' , price:'60', available:'Not Available',image:'/assets/Products_Images/carrot.jpg'},
-    {id:4, name: 'Floor' , price:'25', available:'Available',image:'/assets/Products_Images/floor.webp'},
-    {id:5, name: 'Honey' , price:'45', available:'Not Available',image:'/assets/Products_Images/honey.jpg'},
-    {id:6, name: 'Milk' , price:'15', available:'Available',image:'/assets/Products_Images/milk.jpg'},
-    {id:7, name: 'Mixed Vegetable' , price:'22', available:'Available',image:'/assets/Products_Images/mixed-veg.webp'},
-    {id:8, name: 'Potato' , price:'8', available:'Available',image:'/assets/Products_Images/potato.jpg'},
-    {id:9, name: 'Tomato' , price:'6', available:'Not Available',image:'/assets/Products_Images/tomato.webp'},
-    {id:10, name: 'Rice' , price:'12', available:'Not Available',image:'/assets/Products_Images/rice.jpg'}
+    {id:1, name: 'Apple' , price:'50', availability:'Available',image:'/assets/Products_Images/apple.jpg'},
+    {id:2, name: 'Egg' , price:'20', availability:'Available',image:'/assets/Products_Images/eggs.webp'},
+    {id:3, name: 'Carrot' , price:'60', availability:'Not Available',image:'/assets/Products_Images/carrot.jpg'},
+    {id:4, name: 'Floor' , price:'25', availability:'Available',image:'/assets/Products_Images/floor.webp'},
+    {id:5, name: 'Honey' , price:'45', availability:'Not Available',image:'/assets/Products_Images/honey.jpg'},
+    {id:6, name: 'Milk' , price:'15', availability:'Available',image:'/assets/Products_Images/milk.jpg'},
+    {id:7, name: 'Mixed Vegetable' , price:'22', availability:'Available',image:'/assets/Products_Images/mixed-veg.webp'},
+    {id:8, name: 'Potato' , price:'8', availability:'Available',image:'/assets/Products_Images/potato.jpg'},
+    {id:9, name: 'Tomato' , price:'6', availability:'Not Available',image:'/assets/Products_Images/tomato.webp'},
+    {id:10, name: 'Rice' , price:'12', availability:'Not Available',image:'/assets/Products_Images/rice.jpg'}
   ]
+
+  getAllProducts(){
+    return this.products.length;
+  }
+
+  getAvailableProducts(){
+    return this.products.filter(products => products.availability === 'Available').length;
+  }
+
+  getNotAvailableProducts(){
+    return this.products.filter(products => products.availability === 'Not Available').length;
+  }
+
+  productCountRadioButton:string = 'All';
+
+  onFilterRadioButtonChange(data : string){
+    this.productCountRadioButton = data;
+    console.log(this.productCountRadioButton);
+  }
 
 }
